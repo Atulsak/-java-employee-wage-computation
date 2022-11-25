b54d3016107731fbb4f36eba2860f2827e9e8078
Atulsak <sakhareat@gmail.com>
2022-11-25 16:33:45 +0530

    ADDED: employee attendance

--- src/com/Employeewage/EmployeeWage.java
+++ src/com/Employeewage/EmployeeWage.java
@@ -1,7 +1,22 @@
 package com.Employeewage;
 
 public class EmployeeWage {
- public static void main(String[] args) {
-  System.out.println("Welcome to Employee Wage Computation");
-}
+  public final static int IS_PRESENT = 1;
+		
+	public static int attendance() {
+		if ((int) Math.floor(Math.random() * 10) % 2 == IS_PRESENT) {
+				System.out.println("Emplyee is present.");
+				return IS_PRESENT;
+	}  
+		else {
+				System.out.println("Employee is absenet.");
+				return 0;
+			}
+		}
+		
+		public static void main(String[] args) {
+			System.out.println("Welcome to Employee Wage Computation Program");
+			attendance();
+		}
+
 }
