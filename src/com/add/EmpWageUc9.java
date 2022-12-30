7a964e5348712943ca3257ffcfc4b9d7b7909abd
Atulsak <sakhareat@gmail.com>
2022-12-30 21:35:20 +0530

    UC-09-Wage-each-company

---
+++ src/com/add/EmpWageUc9.java
@@ -0,0 +1,61 @@
+package com.add;
+
+public class EmpWageUc9 {
+	 public static final int IS_FULL_TIME = 1;
+	    public static final int IS_PART_TIME = 2;
+//	    static final int EMP_WAGE_PER_HOUR = 20;
+//	    static final int NUM_WORKING_DAYS = 20;
+//	    static  final int MAX_WORKING_HOURS = 100;
+	    private final String company;
+	    private final int empRatePerHour;
+	    private final int numOfWorkingDays;
+	    private final int maxHoursPerMonth;
+	    private int totalEmpWage;
+
+	     public EmpWageUc9 (String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+	        this.company = company;
+	        this.empRatePerHour = empRatePerHour;
+	        this.numOfWorkingDays = numOfWorkingDays;
+	        this.maxHoursPerMonth = maxHoursPerMonth;
+	    }
+	    public void computeEmpWage() {
+
+	        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+	        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays <= numOfWorkingDays){
+	            int empCheck =  (int) (Math.floor(Math.random() * 10) % 2);
+	            totalWorkingDays++;
+	            switch (empCheck) {
+	                case IS_FULL_TIME -> {
+	                    System.out.println("Employee is Present.");
+	                    empHrs = 8;
+	                }
+	                case IS_PART_TIME -> {
+	                    System.out.println("Employee is Part time present");
+	                    empHrs = 4;
+	                }
+	                default -> {
+	                    System.out.println("Employee is Absent");
+	                    empHrs = 0;
+	                }
+	            }
+	            totalEmpHrs += empHrs;
+	            System.out.println("Total Working Hours :: "+empHrs+" \nTotal Working Days ::"+totalWorkingDays);
+	        }
+	        totalEmpWage = totalEmpHrs * empRatePerHour;
+	    }
+
+	 
+		public String toString() {
+	            return "Total Emp Wage for Company :: " +company+ " is :: " +totalEmpWage;
+	    }
+	    public static void main(String[] args) {
+	        System.out.println("Welcome to the Employees Wage ...");
+	        EmpWageUc9 jio = new EmpWageUc9("jio", 20,2, 10);
+	        EmpWageUc9 DMart = new EmpWageUc9("DMart", 10, 4, 20);
+	        jio.computeEmpWage();
+	        System.out.println(jio);
+	        DMart.computeEmpWage();
+	        System.out.println(DMart);
+	    }
+
+}
